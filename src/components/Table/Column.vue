7ec1444b50582c25074c60c4a56e82b6e86c267d
Keifer Gu <keifergu@users.noreply.github.com>
2017-09-02 00:46:03 +0800

    fix(row & column): 修复row和column的结点为div的问题

--- src/components/Table/Column.vue
+++ src/components/Table/Column.vue
@@ -1,20 +1,10 @@
 <template>
-<div>
   <th>{{label}}</th>
-  <slot text="scopeText"></slot>
-</div>
 </template>
 
 <script>
 export default {
   name: 'fix-column',
-  props: ['prop', 'label'],
-  mounted () {
-    let defaultV = this.$scopedSlots.default
-    if (defaultV) {
-      console.log(defaultV)
-      console.log(defaultV())
-    }
-  }
+  props: ['prop', 'label']
 }
 </script>
--- src/components/Table/Row.js
+++ src/components/Table/Row.js
@@ -2,11 +2,10 @@ export default {
   name: 'row',
   functional: true,
   render (h, context) {
-    console.log(context)
     // data 为该列的数据，columns 为列的配置
     const { data, columns } = context.props
-    const row = columns.map(col =>
-      col.template ? col.template({data}) : h('td', data[col.prop]))
-    return h('div', row)
+    const row = columns.map(col => h('td',
+      col.template ? col.template({data}) : data[col.prop]))
+    return row
   }
 }
