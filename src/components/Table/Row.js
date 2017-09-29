87a401a29e63bc53d07be64f6ce54c43f88a8560
Keifer Gu <keifergu@gmail.com>
2017-09-29 16:54:55 +0800

    添加JSX支持

--- src/components/Table/Row.js
+++ src/components/Table/Row.js
@@ -4,12 +4,13 @@ export default {
   render (h, context) {
     // data 为该列的数据，columns 为列的配置
     const { data, columns, index } = context.props
+    console.log(context)
     const row = columns.map(col => {
       switch (col.type) {
         case 'selection':
-          return h('td', [h('input', {attrs: {type: 'checkbox'}})])
+          return <td><input type="checkbox"/></td>
         default:
-          return h('td', col.template ? col.template({data, index}) : data[col.prop])
+          return <td>{col.template ? col.template({data, index}) : data[col.prop]}</td>
       }
     })
     return row
