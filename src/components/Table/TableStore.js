3723008b2da2ded33f092556bc454353b7999c9a
Keifer Gu <keifergu@gmail.com>
2017-10-09 18:20:43 +0800

    完善tablestore

--- src/components/Table/TableStore.js
+++ src/components/Table/TableStore.js
@@ -1,14 +1,18 @@
+import Vue from 'vue'
+
 class TableStore {
   constructor (options) {
     this.state = options.state
     this._mutations = Object.create(null)
+    this._wrappedGetters = Object.create(null)
 
     installStore(this, options)
+
+    restoreVM(this)
   }
 
   commit (type, payload) {
     const entry = this._mutations[type]
-    console.log(payload)
     if (entry) {
       entry.forEach(function commitIterator (handler) {
         handler(payload)
@@ -21,6 +25,9 @@ function installStore (store, options) {
   for (let type in options.mutations) {
     registerMutation(store, type, options.mutations[type])
   }
+  for (let prop in options.getters) {
+    registerGetter(store, prop, options.getters[prop])
+  }
 }
 
 function registerMutation (store, type, handler) {
@@ -28,6 +35,32 @@ function registerMutation (store, type, handler) {
   entry.push(function wrappedMutation (payload) {
     handler.call(store, store.state, payload)
   })
+}
+
+function registerGetter (store, prop, getter) {
+  const entry = store._wrappedGetters[prop] || (store._wrappedGetters[prop] = [])
+  entry.push(function wrappedGetter () {
+    return getter.call(store, store.state)
+  })
+}
+// TODO: getter 属性的响应
+function restoreVM (store) {
+  const state = store.state
+  let computed = {}
+  Object.entries(store._wrappedGetters).forEach(([key, getter]) => {
+    computed[key] = getter[0]
+    Object.defineProperty(store, key, {
+      get: () => store._vm[key],
+      enumerable: true
+    })
+  })
+  store._vm = new Vue({
+    data: {
+      $$state: state
+    },
+    computed
+  })
+  console.log(store._vm)
 }
 
 export default TableStore
--- src/components/Table/store.js
+++ src/components/Table/store.js
@@ -1,20 +1,70 @@
 import TableStore from './TableStore'
 
+export const ROW_CHECKED = Symbol('rowChecked')
+const checkStatus = {
+  all: 'all',
+  part: 'part',
+  empty: 'empty'
+}
+
 const store = new TableStore({
   state: {
-    originData: {},
+    // 原始数据
+    originData: [],
+    // 列的配置
     columns: [],
+    // 右键菜单状态
     menu: {
       visible: false,
       scope: {}
     }
   },
   mutations: {
+    // 更新原始数据
     updateOriginData (state, payload) {
       state.originData = payload.data
     },
+    // 更新列数据，表示列的配置
     updateColumns (state, payload) {
       state.columns = payload.columns
+    },
+
+    updateTableChecked (state, { oldCheckedStatus }) {
+      switch (oldCheckedStatus) {
+        case checkStatus.empty:
+        case checkStatus.part:
+          this.commit('selectAll', true)
+          break
+        case checkStatus.all:
+          this.commit('selectAll', false)
+          break
+        default:
+      }
+    },
+
+    selectAll (state, status) {
+      state.originData.forEach(function (row) {
+        row[ROW_CHECKED] = status
+      })
+    },
+
+    checkRow (state, { key, status }) {
+      const row = state.originData.find(row => row.key === key)
+      row[ROW_CHECKED] = status
+    }
+  },
+  getters: {
+    tableChecked (state) {
+      const originLength = state.originData.length
+      const checkedArray = state.originData.filter(row => row[ROW_CHECKED])
+      switch (checkedArray.length) {
+        case 0:
+          return checkStatus.empty
+        case originLength:
+          return checkStatus.all
+        default:
+          return checkStatus.part
+      }
     }
   }
 })
